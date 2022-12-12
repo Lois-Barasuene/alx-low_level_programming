c298f9351603d9012dff17b7ff2ed5e225566a0e
Lois-Barasuene <www.loisblessingpaul@gmail.com>
2022-12-12 13:34:31 -0800

    a-z

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+* main - The Entry point of the program
+*
+* Return: Always 0 (success)
+*/
+
+
+int main(void)
+
+{
+
+char i;
+{
+for (i = 'a'; i <= 'z'; i++)
+putchar(i);
+}
+putchar('\n');
+
+return (0);
+
+}
