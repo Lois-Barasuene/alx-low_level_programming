be94ee487f2bfdfb9aa2bcfc9feabf3007e24329
Lois-Barasuene <www.loisblessingpaul@gmail.com>
2022-12-14 08:06:56 -0800

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+*main- the entry point
+*
+*Return: Always 0 (success)
+*/
+
+int main(void)
+{
+
+int i;
+{
+for (i = 0; i < 10; ++i)
+printf("%d\n", i);
+}
+
+return (0);
+}
