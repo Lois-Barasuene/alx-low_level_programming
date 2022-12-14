c069a1f32d3ee8b856eda9266d4d5859110d1c66
Lois-Barasuene <www.loisblessingpaul@gmail.com>
2022-12-14 02:46:21 -0800

    printing both cases of the alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+*main - Entry point
+*
+*Return: Always 0 (success)
+*/
+
+int main(void)
+{
+char i;
+char j;
+
+{
+for (i = 'a'; i <= 'z'; i++)
+putchar(i);
+}
+
+{
+for (j = 'A'; j <= 'Z'; j++)
+putchar(j);
+}
+
+putchar('\n');
+return (0);
+}
