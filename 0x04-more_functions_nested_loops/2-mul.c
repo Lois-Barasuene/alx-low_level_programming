9363dd6e2a43faeefee594423e02bcfdd963a5a8
Lois-Barasuene <www.loisblessingpaul@gmail.com>
2023-01-01 18:55:32 -0800

    2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -4,7 +4,7 @@
 *int mul- a function that multiplies two integers
 *@a: parameter for int mul
 *@b: parameter for int mul
-*Return: 0 if successful
+*Return: c if successful
 */
 
 int mul(int a, int b)
