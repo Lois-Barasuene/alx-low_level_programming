3a3a8a87beaa05eb87cbb97609de619d63200584
Lois-Barasuene <www.loisblessingpaul@gmail.com>
2023-01-01 19:51:02 -0800

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,10 +1,13 @@
 #include "main.h"
 
-/*
-*print_numbers- a function that prints the numbers,
-*Description - from 0 to 9, followed by a new line
+/**
+*print_numbers - a function that prints the numbers,
+*from 0 to 9, followed by a new line
 *Return: void
 */
+
+
+
 
 void print_numbers(void)
 {
